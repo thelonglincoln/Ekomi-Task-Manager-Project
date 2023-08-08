405b4207effb2f5ce210b7583351d2c34a705af2
Lincoln <130275392+thelonglincoln@users.noreply.github.com>
2023-08-08 10:37:38 -0700

    Create users.dart

---
+++ lib/backend/supabase/database/tables/users.dart
@@ -0,0 +1,34 @@
+import '../database.dart';
+
+class UsersTable extends SupabaseTable<UsersRow> {
+  @override
+  String get tableName => 'users';
+
+  @override
+  UsersRow createRow(Map<String, dynamic> data) => UsersRow(data);
+}
+
+class UsersRow extends SupabaseDataRow {
+  UsersRow(Map<String, dynamic> data) : super(data);
+
+  @override
+  SupabaseTable get table => UsersTable();
+
+  String get id => getField<String>('id')!;
+  set id(String value) => setField<String>('id', value);
+
+  DateTime? get createdAt => getField<DateTime>('created_at');
+  set createdAt(DateTime? value) => setField<DateTime>('created_at', value);
+
+  String? get firstName => getField<String>('firstName');
+  set firstName(String? value) => setField<String>('firstName', value);
+
+  String? get lastName => getField<String>('lastName');
+  set lastName(String? value) => setField<String>('lastName', value);
+
+  String? get location => getField<String>('location');
+  set location(String? value) => setField<String>('location', value);
+
+  String? get email => getField<String>('email');
+  set email(String? value) => setField<String>('email', value);
+}
